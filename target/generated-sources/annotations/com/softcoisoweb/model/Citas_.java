e29190fbf84eb9cffdf08203dffa979ae8456050
Manuela Jimenez <manuela_jimenez23141@elpoli.edu.co>
2020-04-22 01:16:09 -0500

    make it better

--- target/generated-sources/annotations/com/softcoisoweb/model/Citas_.java
+++ target/generated-sources/annotations/com/softcoisoweb/model/Citas_.java
@@ -4,11 +4,12 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-19T18:07:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-22T01:15:17")
 @StaticMetamodel(Citas.class)
 public class Citas_ { 
 
     public static volatile SingularAttribute<Citas, String> descripcion;
+    public static volatile SingularAttribute<Citas, String> estado;
     public static volatile SingularAttribute<Citas, Integer> ano;
     public static volatile SingularAttribute<Citas, String> titulo;
     public static volatile SingularAttribute<Citas, String> codigoCasoPersona;
--- target/generated-sources/annotations/com/softcoisoweb/model/Usuario_.java
+++ target/generated-sources/annotations/com/softcoisoweb/model/Usuario_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-19T18:07:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-22T01:15:17")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
