c79e64b1200d96664f0f5e402bb8efd8b10a836b
Manuela Jimenez <manuela_jimenez23141@elpoli.edu.co>
2020-06-13 15:22:13 -0500

    se realiza parte final del front end del proceso de calificación, se hace el backe end de la calificación, para que el siguiente paso sea construir el front end

--- target/generated-sources/annotations/com/softcoisoweb/model/Formacion_.java
+++ target/generated-sources/annotations/com/softcoisoweb/model/Formacion_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-12T19:51:17")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-13T15:17:12")
 @StaticMetamodel(Formacion.class)
 public class Formacion_ { 
 
