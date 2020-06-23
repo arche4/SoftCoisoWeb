d2ff3735c9942397dbefd12ec0bd031b140f071a
Manuela Jimenez <manuela_jimenez23141@elpoli.edu.co>
2020-06-23 00:23:57 -0500

    Se realiza el front end de los modulos EPS, AFP, ARL.

--- src/main/webapp/JavaScript/afp.js
+++ src/main/webapp/JavaScript/afp.js
@@ -1,7 +1,172 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(window).load(function () {
+    $(".loader").fadeOut("slow");
+});
+$(document).ready(function () {
+    $('#table_id').dataTable();
+
+    $("body").on("click", "#btnConsultar", function () {
+        $(".loader").fadeIn("slow");
+        var btnConsultar = $(this).val();
+        $.ajax({
+            async: false,
+            type: "GET",
+            url: "/AfpServlet",
+            data: 'btnConsultar=' + btnConsultar,
+            success: function (data) {
+                $(".loader").fadeOut("slow");
+                var respuesta = $.trim(data);
+                if (respuesta !== "" && respuesta !== null) {
+                    respuesta = respuesta.split("#");
+                    $("#codigoMod").val(respuesta[0]);
+                    $("#nombreMod").val(respuesta[1]);
+                    $('#modificarAfp').modal('show');
+                } else {
+                    var cadena = '<div class="form-row">'
+                            + '<h5>Lo sentimos, se ha presentado un problema consultado los datos .</h3>'
+                            + ' </div>';
+                    $('#modInferror').html(cadena);
+                    $('#modalInfError').modal('show');
+                }
+            }
+        });
+
+    });
+});
+
+function myFunctionReload() {
+    location.reload();
+}
+function guardar() {
+    var elmForm = $("#afp");
+    if (elmForm) {
+        elmForm.validator('validate');
+        var elmErr = elmForm.find('.has-error');
+        if (elmErr && elmErr.length > 0) {
+            return false;
+        } else {
+            $(".loader").fadeIn("slow");
+            $('#crearAfp').modal('hide');
+            var btnCrear = 'ok';
+            var codigo = $('#codigo').val();
+            var nombre = $('#nombre').val();
+            $.ajax({
+                async: false,
+                type: "POST",
+                url: "/AfpServlet",
+                data: 'btnCrear=' + btnCrear + '&codigo=' + codigo + '&nombre=' + nombre,
+                success: function (data) {
+                    $(".loader").fadeOut("slow");
+                    if (data === "0") {
+                        var cadena = ' <div class="form-row">'
+                                + '<h5>La AFP   fue guardado con ex\u00EDto.</h3>'
+                                + '</div>';
+                        $('#modInfexito').html(cadena);
+                        $('#modalInfexito').modal('show');
+                    } else if (data === "1") {
+                        var cadena = '<div class="form-row">'
+                                + '<h5>La AFP  ya existe, intente con otro nuevo .</h3>'
+                                + ' </div>';
+                        $('#modInferror').html(cadena);
+                        $('#modalInfError').modal('show');
+                    } else if (data === "2") {
+                        var cadena = '<div class="form-row">'
+                                + '<h5>Lo sentimos, se ha presentado un problema guardando los datos .</h3>'
+                                + ' </div>';
+                        $('#modInferror').html(cadena);
+                        $('#modalInfError').modal('show');
+                    }
+                }});
+        }
+    }
+}
+function validar() {
+    var elmForm = $("#afpMod");
+    if (elmForm) {
+        elmForm.validator('validate');
+        var elmErr = elmForm.find('.has-error');
+        if (elmErr && elmErr.length > 0) {
+            return false;
+        } else {
+            var cadena = ' <div class="form-row">'
+                    + '<h5> ¿ Esta seguro que quieres realizar los cambios ? </h3>'
+                    + ' </div>';
+            $('#InfoConfirmar').html(cadena);
+            $('#modalValidar').modal('show');
+            $('#modificarAfp').modal('hide');
+        }
+    }
+}
+
+function validarEliminar() {
+    var cadena = ' <div class="form-row">'
+            + '<h5> ¿ Esta seguro que quieres eliminar la EPS? </h3>'
+            + ' </div>';
+    $('#InfoEliminar').html(cadena);
+    $('#modalEliminar').modal('show');
+    $('#modificarAfp').modal('hide');
+}
+function modificar() {
+
+    $(".loader").fadeIn("slow");
+    $('#modalValidar').modal('hide');
+    var btnModificar = 'ok';
+    var codigo = $('#codigoMod').val();
+    var nombre = $('#nombreMod').val();
+    $.ajax({
+        async: false,
+        type: "POST",
+        url: "/AfpServlet",
+        data: 'btnModificar=' + btnModificar + '&codigo=' + codigo + '&nombre=' + nombre,
+        success: function (data) {
+            $(".loader").fadeOut("slow");
+            if (data === "Exitoso") {
+                var cadena = ' <div class="form-row">'
+                        + '<h5>Los cambios fueron guardados con ex\u00EDto.</h3>'
+                        + '</div>';
+                $('#modInfexito').html(cadena);
+                $('#modalInfexito').modal('show');
+            } else {
+                var cadena = '<div class="form-row">'
+                        + '<h5>Lo sentimos, se ha presentado un problema guardando los datos .</h3>'
+                        + ' </div>';
+                $('#modInferror').html(cadena);
+                $('#modalInfError').modal('show');
+            }
+        }});
+}
+function eliminar() {
+    $(".loader").fadeIn("slow");
+    $('#modalEliminar').modal('hide');
+    var btnEliminar = $('#codigoMod').val();
+    $.ajax({
+        async: false,
+        type: "POST",
+        url: "/AfpServlet",
+        data: 'btnEliminar=' + btnEliminar,
+        success: function (data) {
+            $(".loader").fadeOut("slow");
+            if (data === "0") {
+                var cadena = ' <div class="form-row">'
+                        + '<h5>La AFP fue eliminadA con ex\u00EDto.</h3>'
+                        + '</div>';
+                $('#modInfexito').html(cadena);
+                $('#modalInfexito').modal('show');
+            } else if (data === "1") {
+                var cadena = '<div class="form-row">'
+                        + '<h5>La AFP  no se puede eliminar, porque esta asociado a una Persona.</h5>'
+                        + '<h5>Intente cambiando el  AFP de la persona.</h5>'
+                        + ' </div>';
+                $('#modInferror').html(cadena);
+                $('#modalInfError').modal('show');
+            } else if (data === "2") {
+                var cadena = '<div class="form-row">'
+                        + '<h5>Lo sentimos, se ha presentado un problema eliminando los datos .</h3>'
+                        + ' </div>';
+                $('#modInferror').html(cadena);
+                $('#modalInfError').modal('show');
+            }
+        }
+    });
+}
 
 
