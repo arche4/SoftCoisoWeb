787c6668fc41ee87d04c546b2405875c64189c05
Manuela Jimenez <manuela_jimenez23141@elpoli.edu.co>
2020-06-25 01:34:19 -0500

    se realizan ajustes a las rutas del sistema.

--- target/generated-sources/annotations/com/softcoisoweb/model/CasoPersona_.java
+++ target/generated-sources/annotations/com/softcoisoweb/model/CasoPersona_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-24T23:16:00")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-25T01:25:19")
 @StaticMetamodel(CasoPersona.class)
 public class CasoPersona_ { 
 
