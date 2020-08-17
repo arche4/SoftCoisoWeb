046a2335cc7469cdab4ff522410de7a9dc3b4f4e
Manuela Jimenez <manuela_jimenez23141@elpoli.edu.co>
2020-08-17 18:15:22 -0500

    se actualiza la conexión a la bd

---
+++ target/SoftCoisoWeb-V.1/JavaScript/reportes.js
@@ -0,0 +1,62 @@
+$body = $("body");
+$(document).ready(function () {
+     $(".loader").fadeOut("slow");
+     
+    $('#rangTime').daterangepicker({
+        timePicker: true,
+        timePickerIncrement: 1,
+        locale: {
+            format: 'YYYY-MM-DD HH:mm'
+        }
+    });
+
+    $("#btnDescargar").click(function () {
+       window.location = 'http://127.0.0.1:8887//ReportesPersonas.xls';
+    });
+
+    //EL BOTON CONSULTAR EJECUTA  
+    $("body").on("click", "#botCons", function () {
+        $(".loader").fadeIn("slow");
+        var tCons;
+        var fecha_i;
+        var fecha_f;
+        var btnReporte = 'ok';
+        event.stopPropagation();
+        event.preventDefault();
+        tCons = $('#selectConsulta option:selected').text();
+        fecha_i = ($('#rangTime').val()).substring(0, 16);
+        fecha_f = ($('#rangTime').val()).substring(19, 41);
+        $.when($.ajax({
+            type: "POST",
+            url: "/SoftCoisoWeb/ReportesServlet",
+            data: 'tCons=' + tCons + '&fecha_i=' + fecha_i + '&fecha_f=' + fecha_f + '&btnReporte=' + btnReporte,
+            success: function (data) {
+                console.log(data);
+            }
+        })).done(function (data) {
+            $(".loader").fadeOut("slow");
+            $("#btnDescargar").attr("urlDescarga", tCons + ".xls");
+            $("#btnDescargar").prop("disabled", false);
+            $body.removeClass("loading");
+            $('#miTabla').html(data);
+            var idTabla = $('#miTabla table').attr("id");
+            $("#" + idTabla).DataTable({
+                "language": {
+                    "lengthMenu": "Mostrar _MENU_ registros por p&aacute;gina",
+                    "zeroRecords": "No se encontraron datos ",
+                    "info": "Mostrando p&aacute;gina _PAGE_ de _PAGES_",
+                    "infoEmpty": "No hay registros disponibles",
+                    "infoFiltered": "(Filtrados de un total de _MAX_ registros)",
+                    "search": "Buscar registro:",
+                    "pagingType": {
+                        "next": "Siguiente",
+                        "previous": "Anterior"
+                    },
+                    "loadingRecords": "Cargando..."
+                },
+
+            });
+
+        });
+    });
+});
